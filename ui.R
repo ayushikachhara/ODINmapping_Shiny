c99df9896ef5b9d357a2b707d75a7e17f7705993
ayushikachhara <kachhara.aayushi@gmail.com>
2017-06-25 11:33:15 +1200

    changed the page settings, added a sidebar, a text output and a plotly output.

--- ui.R
+++ ui.R
@@ -10,17 +10,21 @@ source('./constants_and_paths.R')
 
 # Load data
 source('./load_data.R')
+
 # The user interface
-ui <- fluidPage(
-  titlePanel("Community Observations Network for Air - 2016"),
-  h3("Month of August 2016, Rangiora"),
-  sidebarLayout(sliderInput("timeRange", label = "Date/Time:",
-                  min = min(data$date_time3),
-                  max = max(data$date_time3),
-                  value = min(data$date_time3),
-                  step = 600,
-                  animate = animationOptions(interval = 500,
-                                             loop = FALSE)),
-                mainPanel(leafletOutput("myMap"),
-                          plotOutput("myPlot")))
+ui <- pageWithSidebar(
+  headerPanel("Community Observations Network for Air - 2016"),
+  sidebarPanel(
+    
+    sliderInput("timeRange", label = "Date/Time:",
+                min = min(data$date_time3),
+                max = max(data$date_time3),
+                value = min(data$date_time3),
+                step = 600,
+                animate = animationOptions(interval = 500,
+                                           loop = FALSE)),
+    h6(textOutput("timeRange")),
+    plotOutput("myPlot")
+    ),
+  mainPanel(plotlyOutput("plotly", height = "300px"),leafletOutput("myMap", height = "300px"))
   )
