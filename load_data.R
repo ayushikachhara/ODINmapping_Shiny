778fe1f811aa9412b1fd3e5f51d8f05b8484d681
ayushikachhara <kachhara.aayushi@gmail.com>
2017-10-19 10:49:07 +1300

    dealing with the timezones because of the 'UTC' issue

--- load_data.R
+++ load_data.R
@@ -1,13 +1,26 @@
 #load data
 ## Get DUST data
 load('./dust_data.RData')
-binpal <- colorBin(c("Green","Orange","Red"),c(0,140), 8, pretty = TRUE, na.color = "Black")
+data$date_time3 <- with_tz(data$date_time3, "UTC")
+data$date_time3 <- data$date_time3 +43200
+binpal <- colorBin(c("Green","Orange","Red"),c(0,140), 8, 
+                   pretty = TRUE, na.color = "Black")
+
 date_vec <- seq(from = min(data$date_time3), to = max(data$date_time3),by = 60)
+
+## load raster
 load('./raster_odin.RData')
-#load('./raster_odin.RData')
-#l_rast <- c('D0','D1')
+
 # Get WIND data
 load('./wind_data1.RData')
 
+sp.lines.df$w.date <- with_tz(sp.lines.df$w.date, "UTC")
+sp.lines.df$w.date <- sp.lines.df$w.date +43200
+
 #Get Ecan data
 load('./ecan_data.RData')
+data_ecan$DateTime<- with_tz(data_ecan$DateTime, "UTC")
+data_ecan$DateTime <- data_ecan$DateTime +43200
+
+minimum.slider <-  data$date_time3[1]
+maximum.slider <-  data$date_time3[length(data$date_time3)]
--- load_ecan_data.R
+++ load_ecan_data.R
@@ -1,8 +1,22 @@
 data_ecan <- read.csv("./Data/August_Ecan.csv")
-# data_ecan$DateTime<- as.POSIXct(strptime(as.character(data_ecan$DateTime),
-#                                          format = "%d/%m/%y %H:%M", tz = "Pacific/Auckland"))
-data_ecan <- data_ecan[which(data_ecan$DateTime>"2016-08-11 23:59:00"),]
+data_ecan$NZTM_E <- rep(1571534, length(data_ecan$u))
+data_ecan$NZTM_N <- rep(5204311, length(data_ecan$u))
+
+x <- data_ecan
+coordinates(x) <- ~NZTM_E+NZTM_N
+proj4string(x) <- CRS(NZTM_CRS)
+y <- spTransform(x,CRS("+proj=longlat"))
+y$nztm_E <- data_ecan$NZTM_E
+y$nztm_N <- data_ecan$NZTM_N
+y$DateTime<- as.POSIXct(strptime(as.character(y$DateTime),
+                                         format = "%d/%m/%y %H:%M", tz = "Pacific/Auckland"))
+
+y <- y[which(y$DateTime>"2016-08-11 23:59:00"),]
+
+writeOGR(y, ".","Data/data_ecan", "ESRI Shapefile")
+
+data_ecan <- y
 save(data_ecan,file = './ecan_data.RData')
 
 
-#data_ecan$DateTime2<- as.POSIXct(as.character(data_ecan$DateTime),format = "%d/%m/%y %H:%M")
+
