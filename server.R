de26fdf7777fa8060eadd001385dc18f7a3c0421
ayushikachhara <kachhara.aayushi@gmail.com>
2017-10-19 10:48:12 +1300

    adding more functionality to the slider using 'switch' with 'speed control'
    UpdateSliderInput function to stop the pointer to go the start when there is a change in controls.
    
    Ordering the barplot as East to West to see the correlations.
    
    removing the raster due to errors.

--- server.R
+++ server.R
@@ -8,126 +8,96 @@
 #
 library(shiny)
 
+sliderType <- function(type, steps) {
+  switch(type,
+         Slow = sliderInput("timeRange", label = "Date/Time:", width = '600px',
+                            min = minimum.slider,
+                            max = maximum.slider,
+                            value = minimum.slider,
+                            step = 60*steps,
+                            timezone = "UTC",
+                            animate = animationOptions(interval = 3000,
+                                                       playButton = tags$img(height = 40,width = 45,
+                                                                             src = "https://png.icons8.com/color/1600/circled-play", 
+                                                                             tags$p(tags$b("PLAY"))),
+                                                       pauseButton = tags$img(height = 40, width = 45,
+                                                                              src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
+                                                                              tags$p(tags$b("PAUSE"))))
+         ),
+         Medium = sliderInput("timeRange", label = "Date/Time:", width = '600px',
+                              min = minimum.slider,
+                              max = maximum.slider,
+                              value = minimum.slider,
+                              step = 60*steps,
+                              timezone = "UTC",
+                              animate = animationOptions(interval = 1000,
+                                                         playButton = tags$img(height = 40,width = 45,
+                                                                               src = "https://png.icons8.com/color/1600/circled-play", 
+                                                                               tags$p(tags$b("PLAY"))),
+                                                         pauseButton = tags$img(height = 40, width = 45,
+                                                                                src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
+                                                                                tags$p(tags$b("PAUSE"))))
+         ),
+         Fast = sliderInput("timeRange", label = "Date/Time:", width = '600px',
+                            min = minimum.slider,
+                            max = maximum.slider,
+                            value = minimum.slider,
+                            step = 60*steps,
+                            timezone = "UTC",
+                            animate = animationOptions(interval = 500,
+                                                       playButton = tags$img(height = 40,width = 45,
+                                                                             src = "https://png.icons8.com/color/1600/circled-play", 
+                                                                             tags$p(tags$b("PLAY"))),
+                                                       pauseButton = tags$img(height = 40, width = 45,
+                                                                              src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
+                                                                              tags$p(tags$b("PAUSE"))))
+         ))
+  }
+
+
 # Define server logic 
-server <- function(input,output) {
+server <- function(input,output,session) {
+  
+  observeEvent(input$speed,{
+      val <- input$timeRange 
+      updateSliderInput(session, "timeRange",value=val)
+    
+})
+  observeEvent(input$step_size,{
+    val <- input$timeRange 
+    updateSliderInput(session, "timeRange",value=val)
+  })
   
   ##datetime slider definition.
-  output$slider <- renderUI({
-    
-    sliderInput("timeRange", label = "Date/Time:", width = '300px',
-                min = min(data$date_time3),
-                max = max(data$date_time3),
-                value = min(data$date_time3),
-                step = (60*input$step_size),
-                animate = animationOptions(interval = 3200 - (input$speed*300),
-                                           playButton = tags$img(height = 40,width = 45,
-                                                                 src = "https://png.icons8.com/color/1600/circled-play", 
-                                                                 tags$p(tags$b("PLAY"))),
-                                           pauseButton = tags$img(height = 40, width = 45,
-                                                                  src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
-                                                                  tags$p(tags$b("PAUSE"))))
-                )
-    
+  output$slider <- renderUI({ 
+    sliderType(input$speed, input$step_size)
   })
+
   
-  ##animation speed control
-  output$speed_value <- renderUI({
-    sliderInput("speed",
-                label = div(style='width:300px;',
-                            div(style='float:left;', 'slower'),
-                            div(style='float:right;', 'faster')),
-                min = 1, 
-                max = 10, 
-                value = 6, 
-                width = '300px')
-                
-  })
-  
-  ##time step definition by user
-  output$step_size <- renderUI({
-    sliderInput("step_size","Time Steps (minutes):",
-                min = 10, max = 60, value = 1, width = '300px')
-  })
   
   ##subsetting dust data
   subsetData <- reactive({
-    x <- data[which(data$date_time3 == "2016-08-12 NZST" & !is.na(data$PM2_5)),]
-    order.x <- c(107,103,104,114,113,102,112,100,101,108,106)
+    x <- data[which(data$date_time3 == input$timeRange & !is.na(data$PM2_5)),]
+    order.x <- c(103,110,107,115,114,104,113,106,102,101,109,112,100,105,108)
     x <- x[order(match(x$ODIN,order.x)),]
     x$ODIN <- as.character(x$ODIN)
     return(x)
   
   })
   
-  ##subsetting dust data
-  subsetRaster <- reactive({
-    idx <- which(date_vec == input$timeRange)
-    return(full_raster[[idx]])
-  })
-  
+
   #subsetting wind_data
   filteredData <- reactive({
     return(sp.lines.df[sp.lines.df@data$w.date == input$timeRange,])
   })
   
-  ###datetime output
-  output$selectedtime <- renderText({
-       paste("Animation running from:   ", strftime(input$timeRange, "%d-%m-%Y  %H:%M:%S"))
-  })
-  
-  ### barplot
-  output$myPlot <- renderPlot({
-    barplot(subsetData()$PM2_5,
-            main = "ODIN Readings",
-            xlab = "ODIN ID",
-            ylab = "PM2.5 [ug/m3]",
-            ylim=c(min(data$PM2_5, na.rm = T), 
-                   max(data$PM2_5, na.rm = T)),
-            names.arg = subsetData()$ODIN,
-            col = "#F39C12",
-            border = "black")
-  })
-  
-  ##output with plotly data
-  
-  ##secondary y-axis definition.
-  output$plotly <-renderPlotly({
-    
-    ##need to modify data_ecan here since .RData when read in, does not 
-    ##remember the Datetime format and notes the time as UTC.
-    
-    data_ecan$DateTime <- as.POSIXct(strptime(as.character(data_ecan$DateTime),
-                        format = "%d/%m/%y %H:%M", tz = "Pacific/Auckland"))
-    
-    data_ecan <- data_ecan[which(data_ecan$DateTime>"2016-08-11 23:59:00"),]
-    ## defining the timezone for plot_ly
-    second_axis <- list(
-      tickfont = list(color = "#636363"),
-      overlaying = "y",
-      side = "right",
-      title = "WSpeed",
-      showgrid = F
-    )
-    
-    ##creating the plotly line plot.
-    plot_ly(data_ecan) %>%
-      add_lines(x = ~DateTime, y = ~PM10, name = "PM10", color = I("#F39C12")) %>%
-      
-      add_lines(x = ~DateTime, y = ~u, name = "WSpeed",  yaxis = "y2", color =I("#2471A3")) %>%
-      layout(
-        title = "Ecan_Data", yaxis2 = second_axis,
-        xaxis = list(range = c(input$timeRange,max(data_ecan$DateTime)),
-                     rangeselector = list(buttons = list(
-                       list(count = 7, label = "last week",step = "day", stepmode = "forward"),
-                       list(count = 14,label = "last 2 weeks",step = "day", stepmode = "forward"),
-                       list(step = "all"))),
-                     rangeslider = list(type = "date"), title = "")) %>% 
-      config(displayModeBar = FALSE)
-  })
+############### OUTPUTS ############################
   
   ## ODIN static map.
   output$myMap <- renderLeaflet({
-    leaflet() %>% addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+    
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
       addTiles(group = "Open Street Map") %>%
       addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
       fitBounds(data@bbox[1,1],
@@ -144,32 +114,76 @@ server <- function(input,output) {
   })
   
   ## ODIN dynamic map.
+  
+  ##datetime output:
     observe({
-      leafletProxy('myMap') %>%
+      output$selectedtime <- renderText({
+        paste(format(input$timeRange))
+      })
+      
+      ### barplot
+      output$myPlot <- renderPlot({
+        barplot(subsetData()$PM2_5,
+                main = "ODIN Readings",
+                xlab = "ODIN ID",
+                ylab = "PM2.5 [ug/m3]",
+                ylim=c(min(data$PM2_5, na.rm = T), 
+                       max(data$PM2_5, na.rm = T)),
+                names.arg = subsetData()$ODIN,
+                col = "#F39C12",
+                border = "black")
+      })
+      
+      
+      output$plotly <-renderPlotly({
+        data_ecan <- as.data.frame(data_ecan)
+        ##secondary y-axis definition.
+        second_axis <- list(
+          tickfont = list(color = "#636363"),
+          overlaying = "y",
+          side = "right",
+          title = "WSpeed",
+          showgrid = F
+        )
+        
+## creating the plotly line plot.
+        plot_ly(data_ecan) %>%
+          add_lines(x = ~DateTime, y = ~PM10, name = "PM10", color = I("#F39C12")) %>%
+          add_lines(x = ~DateTime, y = ~u, name = "WSpeed",  yaxis = "y2", color =I("#2471A3")) %>%
+          layout(
+            shapes = list(
+              list(type = "line",
+                   fillcolor = "black", line = list(color = "black"), opacity = 1,
+                   x0 = input$timeRange, x1 = input$timeRange, xref = "x",
+                   y0 = 0, y1 = 140, yref = "y")),
+            title = "Ecan_Data", yaxis2 = second_axis,
+            xaxis = list(range = c(input$timeRange -129600,input$timeRange +129600),
+                         rangeslider = list(type = "date"), title = "")) %>% 
+          config(displayModeBar = FALSE)
+      })
+  
+      
+      leafletProxy('myMap', deferUntilFlush = FALSE) %>% 
       clearGroup('B') %>%
-      addCircleMarkers(data = subsetData(),
+        addCircleMarkers(data = subsetData(),
                        group = 'B',
                        color = ~binpal(PM2_5),
                        radius = 5,
                        label = ~paste("ODIN",as.character(ODIN)),
                        stroke = FALSE,
                        fillOpacity = 1) %>%
-      addPolylines(data = filteredData(),
-                   group = 'B',
-                   opacity=1,
-                   weight = 3,
-                   color = "black") %>%
+        addPolylines(data = filteredData(),
+                     group = 'B',
+                     opacity=1,
+                     weight = 3,
+                     color = "black") %>%
         addLabelOnlyMarkers(data=subsetData(),
                             group = "show labels",
                             label=~paste("ODIN",as.character(ODIN)),
                             labelOptions = labelOptions(noHide = T,
-                                                        direction = 'auto')) %>%
-        addRasterImage(data = subsetRaster(),
-                       group = 'R',
-                       color = binpal,
-                       opacity = 0.75,
-                       project = FALSE)
-      
+                                                         direction = 'auto'))
+        # print(rasterdata)
+      # print(class(input$timeRange))
 
   })
 }
