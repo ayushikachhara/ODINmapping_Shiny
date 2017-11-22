f95019f13863e6f6862f14601b7e453f0b6c960f
Ayushi <kachhara.aayushi@gmail.com>
2017-11-23 10:43:07 +1300

    install libraries if not present.

--- load_libraries.R
+++ load_libraries.R
@@ -1,3 +1,14 @@
+all.libraries <- c('leaflet', 'maps','mapproj',
+                   'maptools','RColorBrewer', 'shiny',
+                   'shinyjs','shinythemes','rgdal',
+                   'sp', 'raster', 'mapview', 'automap',
+                   'dygraphs','xts', 'plotly', 'lubridate')
+
+if(length(new.pkgs <- setdiff(all.libraries, 
+                              rownames(installed.packages())))) install.packages(new.pkgs)
+
+
+
 library(leaflet)
 library(maps)
 library(mapproj)
@@ -13,5 +24,5 @@ library(mapview)
 library(automap)
 library(dygraphs)  ## new addition on 09/05/2017
 library(xts)
-library(plotly) 
+library(plotly)
 library(lubridate)
