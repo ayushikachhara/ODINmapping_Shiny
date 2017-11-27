5742bfbda9ccd27cfacb967ea0ddb7bb4b526e1b
Ayushi <kachhara.aayushi@gmail.com>
2017-11-27 15:05:36 +1300

    no NZTM conversion needed - need to check the timezone.

--- load_data.R
+++ load_data.R
@@ -3,7 +3,7 @@
 load('./dust_data10min.RData')
 data$date_time3 <- as.POSIXct(as.character(data$date_time3), format = "%Y-%m-%d %H:%M:%S")
 data$date_time3 <- with_tz(data$date_time3, "UTC")
-data$date_time3 <- data$date_time3 +43200
+data$date_time3 <- data$date_time3 #+43200
 
 date_vec <- seq(from = min(data$date_time3), to = max(data$date_time3),by = 600)
 
@@ -16,7 +16,7 @@ load('./raster_odinbrick.RData')
 load('./wind_data10min.RData')
 
 sp.lines.df$w.date <- with_tz(sp.lines.df$w.date, "UTC")
-sp.lines.df$w.date <- sp.lines.df$w.date +43200
+sp.lines.df$w.date <- sp.lines.df$w.date #+43200
 
 #Get Ecan data
 load('./ecan_datatable.RData')
@@ -24,7 +24,7 @@ load('./ecan_datatable.RData')
 data_ecan <- data_ecan.table
 
 data_ecan$DateTime<- with_tz(data_ecan$DateTime, "UTC")
-data_ecan$DateTime <- data_ecan$DateTime +43200
+data_ecan$DateTime <- data_ecan$DateTime #+43200
 
 minimum.slider <-  data$date_time3[1]
 maximum.slider <-  data$date_time3[length(data$date_time3)]
