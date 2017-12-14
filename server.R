e5ddaecdcea8888a1ad2dc438a9d7059a3d9ec62
Ayushi Kachhara <kachhara.aayushi@gmail.com>
2017-12-15 09:49:50 +1200

    changing the reactivity to change the invalidation stages for slider input

--- server.R
+++ server.R
@@ -8,61 +8,64 @@
 #
 library(shiny)
 
-##### define a function to generate slider at each radiobutton input. #######
-sliderType <- function(type, steps) {
-  switch(type,
-         Slow = sliderInput("timeRange", label = "Date/Time:", width = '600px',
-                            min = minimum.slider,
-                            max = maximum.slider,
-                            value = minimum.slider,
-                            step = 60*steps,
-                            timezone = "UTC",
-                            animate = animationOptions(interval = 3000,
-                                                       playButton = tags$img(height = 40,width = 45,
-                                                                             src = "https://png.icons8.com/color/1600/circled-play",
-                                                                             tags$p(tags$b("PLAY"))),
-                                                       pauseButton = tags$img(height = 40, width = 45,
-                                                                              src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
-                                                                              tags$p(tags$b("PAUSE"))))
-         ),
-         Medium = sliderInput("timeRange", label = "Date/Time:", width = '600px',
-                              min = minimum.slider,
-                              max = maximum.slider,
-                              value = minimum.slider,
-                              step = 60*steps,
-                              timezone = "UTC",
-                              animate = animationOptions(interval = 1000,
-                                                         playButton = tags$img(height = 40,width = 45,
-                                                                               src = "https://png.icons8.com/color/1600/circled-play",
-                                                                               tags$p(tags$b("PLAY"))),
-                                                         pauseButton = tags$img(height = 40, width = 45,
-                                                                                src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
-                                                                                tags$p(tags$b("PAUSE"))))
-         ),
-         Fast = sliderInput("timeRange", label = "Date/Time:", width = '600px',
-                            min = minimum.slider,
-                            max = maximum.slider,
-                            value = minimum.slider,
-                            step = 60*steps,
-                            timezone = "UTC",
-                            animate = animationOptions(interval = 500, loop = TRUE,
-                                                       playButton = tags$img(height = 40,width = 45,
-                                                                             src = "https://png.icons8.com/color/1600/circled-play",
-                                                                             tags$p(tags$b("PLAY"))),
-                                                       pauseButton = tags$img(height = 40, width = 45,
-                                                                              src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
-                                                                              tags$p(tags$b("PAUSE"))))
-         ))
-  }
-
-
-#### Define server logic #######
-server <- function(input,output,session) {
-  ### update sliderinput based on the various other inputs ####
+#### server definition ####
+server <- function(input, output, session){
+  
+  #### making slider reactive to everytime the step size changes #######
+  steps <- reactive({
+    as.numeric(input$step_size)
+  })
+  
+  #### making slider reactive to everytime the speed of the animation changes ######
+  speedInput <- reactive({
+    switch(input$speed,
+           "Slow" = 3000,
+           "Medium" = 1000,
+           "Fast" = 500)
+  })
+  
+  ## slider definition ####
+  output$slider <- renderUI({
+    sliderInput("timeRange", label = "Date/Time:", width = '600px',
+                min = minimum.slider,
+                max = maximum.slider,
+                value = minimum.slider,
+                step = 60*steps(),
+                timezone = "UTC",
+                animate = animationOptions(interval = speedInput(),
+                                           playButton = tags$img(height = 40,width = 45,
+                                                                 src = "https://png.icons8.com/color/1600/circled-play",
+                                                                 tags$p(tags$b("PLAY"))),
+                                           pauseButton = tags$img(height = 40, width = 45,
+                                                                  src = "https://cdn2.iconfinder.com/data/icons/perfect-flat-icons-2/512/Pause_button_play_stop_blue.png",
+                                                                  tags$p(tags$b("PAUSE"))))
+    )
+  })
+  
+  ## updates to the slider - so to retain the value of the #########
+  ## slider if any of the controls are changed
+  
+  ## date changes ####
+  observe({
+    input$date
+    val <- paste(input$date, "00:00:00")
+    val <- as.POSIXct(val, format = "%Y-%m-%d %H:%M:%S")
+    updateSliderInput(session, "timeRange",value=val)
+  })
+  
+  #####step size changes######
+  observeEvent(input$step_size,{
+    val <- input$timeRange
+    updateSliderInput(session, "timeRange",value=val)
+  })
+  
+  ### speed of animation changes #######
   observeEvent(input$speed,{
-      val <- input$timeRange
-      updateSliderInput(session, "timeRange",value=val)
-      })
+    val <- input$timeRange
+    updateSliderInput(session, "timeRange",value=val)
+  })
+  
+  #### all the action buttons #######
   observeEvent(input$`3hour_f`,{
     val <- input$timeRange + 10800
     updateSliderInput(session, "timeRange",value=val)
@@ -103,27 +106,6 @@ server <- function(input,output,session) {
     updateSliderInput(session, "timeRange",value=val)
   })
   
-  
-  observeEvent(input$step_size,{
-    val <- input$timeRange
-    updateSliderInput(session, "timeRange",value=val)
-  })
-  
-  
-  observe({
-    input$date
-    val <- paste(input$date, "00:00:00")
-    val <- as.POSIXct(val, format = "%Y-%m-%d %H:%M:%S")
-    updateSliderInput(session, "timeRange",value=val)
-  })
-  
-# #
-### datetime slider definition.######
-  output$slider <- renderUI({
-  sliderType(input$speed, input$step_size)
-    })
-  
-  ##### subset raster #####
   subsetRaster <- reactive({
     req(input$timeRange)
     idx <- which(date_vec == input$timeRange)
@@ -139,16 +121,16 @@ server <- function(input,output,session) {
     x <- x[order(match(x$ODIN,order.x)),]
     x$ODIN <- as.character(x$ODIN)
     return(x)
-  
+    
   })
   
-
+  
   #subsetting wind_data #####
   filteredData <- reactive({
     return(sp.lines.df[sp.lines.df@data$w.date == input$timeRange,])
   })
   
-############### OUTPUTS ############################
+  ############### OUTPUTS ############################
   
   # ODIN static map.#########
   output$myMap <- renderLeaflet({
@@ -158,11 +140,11 @@ server <- function(input,output,session) {
                 data@bbox[1,2],
                 data@bbox[2,2]) %>%
       leaflet::addLegend(position = "bottomleft",
-                pal = binpal,
-                values = data$PM2_5, na.label = "not active") %>%
+                         pal = binpal,
+                         values = data$PM2_5, na.label = "not active") %>%
       addLayersControl(baseGroups = c("Toner", "Toner Lite", "Open Street Map"),
-        overlayGroups = c("ODIN_sites","PM2.5(interpolated)","Wind", "show labels"),
-        options = layersControlOptions(collapsed = FALSE)) %>%
+                       overlayGroups = c("ODIN_sites","PM2.5(interpolated)","Wind", "show labels"),
+                       options = layersControlOptions(collapsed = FALSE)) %>%
       hideGroup("show labels")
   })
   
@@ -200,8 +182,8 @@ server <- function(input,output,session) {
   ###### plotly output ####
   output$plotly <-renderPlotly({
     invalidateLater(5000,session)
-
-
+    
+    
     ##secondary y-axis definition.
     second_axis <- list(
       tickfont = list(color = "#636363"),
@@ -210,7 +192,7 @@ server <- function(input,output,session) {
       title = "WSpeed",
       showgrid = F
     )
-
+    
     ## creating the plotly line plot.
     p <- plot_ly(data_ecan) %>%
       add_lines(x = ~DateTime, y = ~PM10, name = "PM10", color = I("#F39C12")) %>%
@@ -225,33 +207,33 @@ server <- function(input,output,session) {
         xaxis = list(range = c(input$timeRange -129600,input$timeRange +129600),
                      rangeslider = list(type = "date"), title = "")) %>%
       config(displayModeBar = FALSE)
-	p$elementId <- NULL
-	p
+    p$elementId <- NULL
+    p
   })
   
   ### datetime output: #####
   output$selectedtime <- renderText({
     paste(format(input$timeRange))
   })
-
+  
   
   ## ODIN dynamic map.####
-    observe({
-     
-      leafletProxy('myMap', deferUntilFlush = FALSE) %>%
-        addProviderTiles(providers$Stamen.Toner, group = "Toner",
-                         options = providerTileOptions(opacity = 1)) %>%
-        addTiles(group = "Open Street Map",
-                 options = tileOptions(opacity = 1)) %>%
-        addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite",
-                         options = providerTileOptions(opacity = 1)) %>%
-        clearGroup('Wind') %>%
-        addRasterImage(subsetRaster(),
-                       group = "PM2.5(interpolated)",
-                       colors = binpal,
-                       project = FALSE,
-                       opacity = 0.70) %>%
-        addCircleMarkers(data = subsetData(),
+  observe({
+    
+    leafletProxy('myMap', deferUntilFlush = FALSE) %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner",
+                       options = providerTileOptions(opacity = 1)) %>%
+      addTiles(group = "Open Street Map",
+               options = tileOptions(opacity = 1)) %>%
+      addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite",
+                       options = providerTileOptions(opacity = 1)) %>%
+      clearGroup('Wind') %>%
+      addRasterImage(subsetRaster(),
+                     group = "PM2.5(interpolated)",
+                     colors = binpal,
+                     project = FALSE,
+                     opacity = 0.70) %>%
+      addCircleMarkers(data = subsetData(),
                        group = 'ODIN_sites',
                        color = "black",
                        weight = 2,
@@ -260,15 +242,16 @@ server <- function(input,output,session) {
                        label = ~paste("ODIN",as.character(ODIN)),
                        stroke = TRUE,
                        fillOpacity = 1) %>%
-        addPolylines(data = filteredData(),
-                     group = 'Wind',
-                     opacity=1,
-                     weight = 3,
-                     color = "black") %>%
-        addLabelOnlyMarkers(data=subsetData(),
-                            group = "show labels",
-                            label=~paste("ODIN",as.character(ODIN)),
-                            labelOptions = labelOptions(noHide = T,
-                                                         direction = 'auto'))
+      addPolylines(data = filteredData(),
+                   group = 'Wind',
+                   opacity=1,
+                   weight = 3,
+                   color = "black") %>%
+      addLabelOnlyMarkers(data=subsetData(),
+                          group = "show labels",
+                          label=~paste("ODIN",as.character(ODIN)),
+                          labelOptions = labelOptions(noHide = T,
+                                                      direction = 'auto'))
   })
+  
 }
