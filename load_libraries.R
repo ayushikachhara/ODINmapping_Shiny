46a5f4b81a843ce14c26e9636621ec5121ee2fe4
Ayushi Kachhara <ayushikachhara@Ayushis-MacBook-Pro.local>
2018-10-02 15:43:13 +1300

    minor changes

--- load_libraries.R
+++ load_libraries.R
@@ -1,17 +1,19 @@
-library(leaflet)
-library(maps)
-library(mapproj)
-library(maptools)
-library(RColorBrewer)
-library(shiny)
-library(shinyjs)
-library(shinythemes)
-library(rgdal)
-library(sp)
-library(raster)
-#library(mapview)
-library(automap)
-library(dygraphs)  ## new addition on 09/05/2017
-library(xts)
-library(plotly)
-library(lubridate)
+library(librarian)
+shelf(leaflet,
+      maps,
+      mapproj,
+      maptools,
+      RColorBrewer,
+      shiny,
+      shinyjs,
+      shinythemes,
+      rgdal,
+      sp,
+      raster,
+      automap,
+      dygraphs,
+      xts,
+      plotly,
+      lubridate)
+
+
